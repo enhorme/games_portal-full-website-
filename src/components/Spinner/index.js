691be4f5d4336561fca0ffa5946a16ac3b00a3c6
Yauheni Pliuta <enhorme@gmail.com>
2022-06-29 18:11:43 +0300

    query params

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.scss";
+
+export default () => {
+  return (
+    <div className="spinner_wrapper">
+      <div className="loadingio-spinner-spin-8evbc9hn5by">
+        <div className="ldio-3we9kvzqixh">
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
